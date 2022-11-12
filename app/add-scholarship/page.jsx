102000e75e2c4eaa9482333da6aef54d1c29c324
Santiago Jaramillo <jaramilloocamposantiago@gmail.com>
2022-11-12 15:15:11 -0500

    Add scholarship

--- app/add-scholarship/page.jsx
+++ app/add-scholarship/page.jsx
@@ -3,30 +3,62 @@ import styles from "../../styles/AddScholarship.module.css";
 export default function AddScholarshipPage() {
     return (
         <section className={styles.container}>
-
+        <h1 className={styles.title}>Publicar beca</h1>
         <form action="" className={styles.form}>
           <label className={styles.formElement}>
-            <span className={styles.formElementTitle}>Nombre</span>
+            <span className={styles.formElementTitle}>Nombre de la beca</span>
             <input
               className={styles.formElementInput}
               type="text"
-              placeholder="Escribe tu nombre aquí..."
+              placeholder="Escribe el nombre de la beca aquí..."
             />
           </label>
           <label className={styles.formElement}>
-            <span className={styles.formElementTitle}>Teléfono</span>
+            <span className={styles.formElementTitle}>Descripción</span>
             <input
               className={styles.formElementInput}
               type="text"
-              placeholder="Escribe tu celular aquí..."
+              placeholder="Escribe la descripción de la beca aquí..."
             />
           </label>
           <label className={styles.formElement}>
-            <span className={styles.formElementTitle}>Correo</span>
+            <span className={styles.formElementTitle}>Link</span>
             <input
               className={styles.formElementInput}
               type="email"
-              placeholder="Escribe tu correo aquí..."
+              placeholder="Escribe el link de la beca aquí..."
+            />
+          </label>
+          <label className={styles.formElement}>
+            <span className={styles.formElementTitle}>Fecha de inicio</span>
+            <input
+              className={styles.formElementInput}
+              type="date"
+              placeholder="Escribe la fecha de inicio de la beca aquí..."
+            />
+          </label>
+          <label className={styles.formElement}>
+            <span className={styles.formElementTitle}>Fecha final</span>
+            <input
+              className={styles.formElementInput}
+              type="date"
+              placeholder="Escribe la fecha final de la beca aquí..."
+            />
+          </label>
+          <label className={styles.formElement}>
+            <span className={styles.formElementTitle}>Institución</span>
+            <input
+              className={styles.formElementInput}
+              type="text"
+              placeholder="Escribe el nombre de la institución aquí..."
+            />
+          </label>
+          <label className={styles.formElement}>
+            <span className={styles.formElementTitle}>Carrera</span>
+            <input
+              className={styles.formElementInput}
+              type="text"
+              placeholder="Escribe el nombre de la carrera aquí..."
             />
           </label>
 
@@ -36,4 +68,5 @@ export default function AddScholarshipPage() {
         </form>
         </section>
     )
+
 }
