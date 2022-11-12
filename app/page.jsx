8e83494078ab34105141366414a15540463b8b2c
Santiago Jaramillo <jaramilloocamposantiago@gmail.com>
2022-11-12 13:00:13 -0500

    Presentation page

--- app/page.jsx
+++ app/page.jsx
@@ -1,20 +1,29 @@
 import React from "react";
 import "../styles/globals.css";
 import styles from "../styles/Home.module.css";
+import Link from "next/link";
 
 export default function HomePage() {
   return (
     <div className={styles.container}>
       <div className={styles.card}>
-        <h2>Presentacion</h2>
+        <h2>Presentación</h2>
         <p>
           A veces, entre tanta información por la que puedes navegar, puede ser
           difícil encontrar la beca indicada para ti. En este sitio te
           ayudaremos a buscar, mediante los filtros que establezcas, esa beca
-          que deseas y que está esperando por ti.
+          que deseas y que está esperando por ti. También, como institución, puedes registrarte y publicar tus propias
+          becas.
         </p>
+        <div className={styles.btnContainer}>
+          <Link className={styles.button} href="/scholarships">
+            Ver becas
+          </Link>
+          <Link className={styles.button} href="/login">
+            Soy institución
+          </Link>
+        </div>
       </div>
-      <div className={styles.card}>Me interesa</div>
     </div>
   );
 }
