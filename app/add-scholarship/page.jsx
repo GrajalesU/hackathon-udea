dba9a51fcd49307479d3d9255b842ba402a97465
Juan Manuel Grajales <juan.grajalesu@gmail.com>
2022-11-12 15:52:52 -0500

    Add functionality

--- app/add-scholarship/page.jsx
+++ app/add-scholarship/page.jsx
@@ -1,72 +1,112 @@
+"use client";
+import json5 from "json5";
+import { useState } from "react";
 import styles from "../../styles/AddScholarship.module.css";
 
 export default function AddScholarshipPage() {
-    return (
-        <section className={styles.container}>
-        <h1 className={styles.title}>Publicar beca</h1>
-        <form action="" className={styles.form}>
-          <label className={styles.formElement}>
-            <span className={styles.formElementTitle}>Nombre de la beca</span>
-            <input
-              className={styles.formElementInput}
-              type="text"
-              placeholder="Escribe el nombre de la beca aquí..."
-            />
-          </label>
-          <label className={styles.formElement}>
-            <span className={styles.formElementTitle}>Descripción</span>
-            <input
-              className={styles.formElementInput}
-              type="text"
-              placeholder="Escribe la descripción de la beca aquí..."
-            />
-          </label>
-          <label className={styles.formElement}>
-            <span className={styles.formElementTitle}>Link</span>
-            <input
-              className={styles.formElementInput}
-              type="email"
-              placeholder="Escribe el link de la beca aquí..."
-            />
-          </label>
-          <label className={styles.formElement}>
-            <span className={styles.formElementTitle}>Fecha de inicio</span>
-            <input
-              className={styles.formElementInput}
-              type="date"
-              placeholder="Escribe la fecha de inicio de la beca aquí..."
-            />
-          </label>
-          <label className={styles.formElement}>
-            <span className={styles.formElementTitle}>Fecha final</span>
-            <input
-              className={styles.formElementInput}
-              type="date"
-              placeholder="Escribe la fecha final de la beca aquí..."
-            />
-          </label>
-          <label className={styles.formElement}>
-            <span className={styles.formElementTitle}>Institución</span>
-            <input
-              className={styles.formElementInput}
-              type="text"
-              placeholder="Escribe el nombre de la institución aquí..."
-            />
-          </label>
-          <label className={styles.formElement}>
-            <span className={styles.formElementTitle}>Carrera</span>
-            <input
-              className={styles.formElementInput}
-              type="text"
-              placeholder="Escribe el nombre de la carrera aquí..."
-            />
-          </label>
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [link, setLink] = useState("");
 
-          <button className={styles.formButton} type="submit">
-            Enviar
-          </button>
-        </form>
-        </section>
-    )
+  const handleSubmit = () => {
+    e.preventDefault();
+    fetch("https://hackaton-udea.herokuapp.com/api/becas", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name,
+        description,
+        link,
+        endDate: "2022/11/12",
+        startDate: "2022/11/12",
+        company: 1,
+        career: "Educación",
+      }),
+    });
+    setDescription("");
+    setLink("");
+    setName("");
+  };
 
+  return (
+    <section className={styles.container}>
+      <h1 className={styles.title}>Publicar beca</h1>
+      <form action="" className={styles.form} onSubmit={handleSubmit}>
+        <label className={styles.formElement}>
+          <span className={styles.formElementTitle}>Nombre de la beca</span>
+          <input
+            className={styles.formElementInput}
+            type="text"
+            placeholder="Escribe el nombre de la beca aquí..."
+            value={name}
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+          />
+        </label>
+        <label className={styles.formElement}>
+          <span className={styles.formElementTitle}>Descripción</span>
+          <input
+            className={styles.formElementInput}
+            type="text"
+            placeholder="Escribe la descripción de la beca aquí..."
+            value={description}
+            onChange={(e) => {
+              setDescription(e.target.value);
+            }}
+          />
+        </label>
+        <label className={styles.formElement}>
+          <span className={styles.formElementTitle}>Link</span>
+          <input
+            className={styles.formElementInput}
+            type="email"
+            placeholder="Escribe el link de la beca aquí..."
+            value={link}
+            onChange={(e) => {
+              setLink(e.target.value);
+            }}
+          />
+        </label>
+        <label className={styles.formElement}>
+          <span className={styles.formElementTitle}>Fecha de inicio</span>
+          <input
+            className={styles.formElementInput}
+            type="date"
+            placeholder="Escribe la fecha de inicio de la beca aquí..."
+          />
+        </label>
+        <label className={styles.formElement}>
+          <span className={styles.formElementTitle}>Fecha final</span>
+          <input
+            className={styles.formElementInput}
+            type="date"
+            placeholder="Escribe la fecha final de la beca aquí..."
+          />
+        </label>
+        <label className={styles.formElement}>
+          <span className={styles.formElementTitle}>Institución</span>
+          <input
+            className={styles.formElementInput}
+            type="text"
+            placeholder="Escribe el nombre de la institución aquí..."
+          />
+        </label>
+        <label className={styles.formElement}>
+          <span className={styles.formElementTitle}>Carrera</span>
+          <input
+            className={styles.formElementInput}
+            type="text"
+            placeholder="Escribe el nombre de la carrera aquí..."
+          />
+        </label>
+
+        <button className={styles.formButton} type="submit">
+          Enviar
+        </button>
+      </form>
+    </section>
+  );
 }
