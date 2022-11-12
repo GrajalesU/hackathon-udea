2be3ec86ef7fee2238d655bad90a1ed2dff46b86
Santiago Jaramillo <jaramilloocamposantiago@gmail.com>
2022-11-12 11:54:53 -0500

    Home preview

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,14 @@
 import React from "react";
+import styles from "../styles/Home.module.css";
 
 export default function HomePage() {
-  return <div>Hola mundo</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.card}>
+        <h2>Presentacion</h2>
+        <p>A veces, entre tanta información por la que puedes navegar, puede ser difícil encontrar la beca indicada para ti. En este sitio te ayudaremos a buscar, mediante los filtros que establezcas, esa beca que deseas y que está esperando por ti.</p>
+        </div>
+      <div className={styles.card}>Me interesa</div>
+    </div>
+  );
 }
