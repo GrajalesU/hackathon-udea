720abc31b42dabc60f7c65de498416d647078171
Juan Manuel Grajales <juan.grajalesu@gmail.com>
2022-11-12 12:27:38 -0500

    Add mocked header

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Roboto } from "@next/font/google";
-
+import styles from "../styles/Header.module.css";
+import Link from "next/link";
 const font = Roboto({
   weight: ["300", "400", "500", "700"],
   subsets: "latin",
@@ -12,7 +13,17 @@ export default function RootLayout({ children }) {
         <title>Los Linces te guían</title>
       </head>
       <body className={font.className}>
-        <header>Header</header>
+        <header className={styles.header}>
+          <h1 className={styles.title}>Los linces te guían</h1>
+          <nav className={styles.navigation}>
+            <Link className={styles.link} href="/scholarships">
+              Becas
+            </Link>
+            <Link className={styles.link} href="/">
+              Principal
+            </Link>
+          </nav>
+        </header>
         {children}
         <footer>Footer</footer>
       </body>
--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "../styles/globals.css";
 import styles from "../styles/Home.module.css";
 
 export default function HomePage() {
@@ -6,8 +7,13 @@ export default function HomePage() {
     <div className={styles.container}>
       <div className={styles.card}>
         <h2>Presentacion</h2>
-        <p>A veces, entre tanta información por la que puedes navegar, puede ser difícil encontrar la beca indicada para ti. En este sitio te ayudaremos a buscar, mediante los filtros que establezcas, esa beca que deseas y que está esperando por ti.</p>
-        </div>
+        <p>
+          A veces, entre tanta información por la que puedes navegar, puede ser
+          difícil encontrar la beca indicada para ti. En este sitio te
+          ayudaremos a buscar, mediante los filtros que establezcas, esa beca
+          que deseas y que está esperando por ti.
+        </p>
+      </div>
       <div className={styles.card}>Me interesa</div>
     </div>
   );
